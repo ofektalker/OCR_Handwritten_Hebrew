d7a6debcad47ac28eef9e227e01c149bc0b13e75
ofektalker <ofekt7144@gmail.com>
2020-12-04 13:13:43 +0200

    get the best params

--- knn_classifier.py
+++ knn_classifier.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2 as cv2
 import os
+import sys
 from sklearn.model_selection import train_test_split
 from skimage import feature
 from skimage import data, exposure
@@ -33,54 +34,73 @@ def pre_process_per_image(path):
 
 def pre_process_directory(path):
     images = []
+    inner_list = []
     labels = []
+    index = 0
     for subdir, dirs, files in os.walk(path):
         labels.append(subdir)
+        images.append(inner_list)
         for filename in files:
             filepath = subdir + os.sep + filename
 
             if filepath.endswith(".jpg") or filepath.endswith(".png"):
                 filepath.replace('/', '\\')
                 img = pre_process_per_image(filepath)
-                images.append(img)
-    labels.pop()
+                images[index].append(img)
+        index = index + 1
+    labels.pop(0)
+    images.pop(0)
     #using np.array to reduce the memory allocation
     labels = np.array(labels)
     images = np.array(images)
     return images, labels
 
 #test_path = "E:/Studies/4/1/Image processing and computer vision/HW2/hhd_dataset/TRAIN/3/3_1.png"
-
-dir_path = "E:/Studies/4/1/Image processing and computer vision/HW2/hhd_dataset/TRAIN/"
+dir_path = "E:/Studies/4/1/Image processing and computer vision/HW2/hhd_dataset/TRAIN"
+#cmd_path = sys.argv[1]
 processed_dataset, labels = pre_process_directory(dir_path)
 print(processed_dataset.shape)
-#processed_dataset = processed_dataset.reshape(processed_dataset.shape[1:])
-print(processed_dataset.shape)
-print(labels.shape)
 
 # split data to train and validation sets
-#train_set, test_set = train_test_split(processed_dataset,labels,train_size=0.9,test_size=0.1, random_state=0)
-# sample = train_set[0]
-# # HOG
-# # TODO: speed up vectorization over for loop
-# HOG_IMAGES = []
-# for image in train_set:
-#     H = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), transform_sqrt=False, block_norm="L2")
-#     HOG_IMAGES.append(H)
+x_train,x_test, y_train, y_test = train_test_split(processed_dataset,labels,train_size=0.9,test_size=0.1, random_state=0)
 
-#K-NN Classifier Train:
-# k is between 1-15
-# distance function are : Euclidean distance , Chi-Square distance
+# HOG
+# TODO: speed up vectorization over for loop
+HOG_train = []
+HOG_test = []
+#preforme HOG over train set and test set
+for image in x_train:
+    H = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), transform_sqrt=False, block_norm="L2")
+    HOG_train.append(H)
+for image in x_test:
+    H = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), transform_sqrt=False,
+                        block_norm="L2")
+    HOG_test.append(H)
+HOG_test = np.array(HOG_test)
+HOG_train = np.array(HOG_train)
+
+
+# reshape to config to Classifier function
+print(HOG_train.shape)
+print(HOG_test.shape)
+#K-NN Classifier:
 
 
 
-# calculating error for K values between 1 to 15 to get the best k value
+#calculating error for K values between 1 to 15 to get the best k value
 # error = []
 # for i in range(1,15):
-#     knn = KNeighborsClassifier(n_neighbors=i)
-#     knn.fit(HOG_IMAGES)
-#     pred_i = knn.predict(test_set)
+#     knn = KNeighborsClassifier(n_neighbors=i, metric="euclidean")
+#     knn.fit(x_train,y_train)
+#     pred_i = knn.predict(x_test)
 #     error.append(np.mean(pred_i != y_test))
+#
+# plt.figure(figsize=(12, 6))
+# plt.plot(range(1, 15), error, color='red', linestyle='dashed', marker='o',
+#          markerfacecolor='blue', markersize=10)
+# plt.title('Error Rate K Value')
+# plt.xlabel('K Value')
+# plt.ylabel('Mean Error')
 
 #   Questions for irina:
 #       1. did we need to do pre-processe to the test data set??
